a2e38475816f115d2ce73547d457880128810c85
lschimm <lschimmelfennig@gmail.com>
2019-05-14 15:15:08 -0500

    working on event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,38 @@
 // Your code goes here
+const btn = document.querySelectorAll('.btn')
+
+btn.forEach(b => {
+b.addEventListener('mouseover', () => {
+    b.style.background = "#FFFFFF";
+})
+})
+
+btn.forEach(c => {
+    c.addEventListener('mouseleave', () => {
+        c.style.background = "#00292d";
+    })
+})
+
+const colorNav = document.querySelectorAll('.main-navigation .container .nav a')
+
+colorNav.forEach(d => {
+    d.addEventListener('mouseout', () => {
+        d.style.color = "#33e3f4";
+    })
+})
+
+colorNav.forEach(d => {
+    d.addEventListener('drag', () => {
+        d.style.color = "#d80000";
+    })
+})
+
+colorNav.forEach(d => {
+    d.addEventListener('pointerover', () => {
+        d.style.color = "#56ffbb";
+    })
+})
+
+
+
+// const img1 = document.querySelector(.'')
